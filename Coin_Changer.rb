e905f580d57ab70f05d73bdc9f4ce794438d3109
TMerrick55 <tmerrick44@gmail.com>
2017-02-15 14:55:03 -0500

    Coin Changer Ver. 3.5

--- Coin_Changer.rb
+++ Coin_Changer.rb
@@ -32,3 +32,33 @@ def quarter_function(cents)
 		quarters = cents / 25
 	end
 end
+
+
+def combined_coin_function(cents)
+
+	coins_hash = {}
+
+	if cents && (cents >= 25)
+		first = quarter_function(cents)
+		cents = cents - (first * 25)
+		coins_hash[:quarter] = first
+	end
+
+	if cents && (cents >= 10)
+		second = dime_function(cents)
+		cents = cents - (second * 10)
+		coins_hash[:dime] = second
+	end
+
+	if cents && (cents >= 5)
+		third = nickel_function(cents)
+		cents = cents - (third * 5)
+		coins_hash[:nickel] = third
+	end
+
+	if cents && (cents >= 1)
+		first = penny_function(cents)
+		coins_hash[:penny] = fourth
+	end
+	coins_hash
+end
